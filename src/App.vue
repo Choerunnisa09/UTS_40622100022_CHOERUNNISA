48c3bb2aa132bc3728a7ee32da125414d59e7edb
choerunnisa09 <ichachoerunnisa098@gmail.com>
2024-05-06 14:28:23 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MhsForm from '@/components/MahasiswaForm.vue'
+import LethNav from '@/components/navbar/LethNav.vue'
+</script>
+
+<template>
+    <v-layout class="rounded rounded-md">
+      <v-app-bar title="Aplikasi penjadwalan kuliah"></v-app-bar>
+
+      <LethNav> </LethNav>
+  
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <RouterView> </RouterView>
+      </v-main>
+    </v-layout>
+  </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TheWelcome from '../components/HomeForm.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
